09a48e05267f688d991220b234cc29f3810eeb3c
Saurabh Grewal <saurabhgrewal718@gmail.com>
2021-05-04 00:43:12 +0530

    firebase database added

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import './pages/home.dart';
 import './themes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
